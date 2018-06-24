3ebcabe382b178fec63d17d334f7d4784dfed327
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2018-06-24 18:53:16 +0200

    Shortcuts functions to make new hasher

---
+++ passwd/hasher.go
@@ -0,0 +1,13 @@
+package passwd
+
+import "github.com/tomi77/go-passwd/hasher"
+
+// NewSHA512Hasher returns new default SHA512 hasher
+func NewSHA512Hasher() hasher.SHA512Hasher {
+	return hasher.SHA512Hasher{}
+}
+
+// NewPlainHasher returns new default plain hasher
+func NewPlainHasher() hasher.PlainHasher {
+	return hasher.PlainHasher{}
+}
