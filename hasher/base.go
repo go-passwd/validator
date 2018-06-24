bdb8c7763a35f9f00301c6e42c07630dd107e83b
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2018-06-24 19:05:43 +0200

    doc

--- hasher/base.go
+++ hasher/base.go
@@ -10,6 +10,7 @@ const (
 	// DefaultSaltLength is a default salt length when is not set manually
 	DefaultSaltLength = 20
 
+	// DefaultIter is a default iterations counter when is not set manually
 	DefaultIter = 2048
 )
 
