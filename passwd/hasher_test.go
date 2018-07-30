89ae7befacdb782928fa7b206b91a10c75d5dee6
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2018-07-30 19:14:16 +0200

    added hasher tests

---
+++ passwd/hasher_test.go
@@ -0,0 +1,62 @@
+package passwd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tomi77/go-passwd/passwd/hasher"
+)
+
+func TestNewSHA512_256Hasher(t *testing.T) {
+	h := NewSHA512_256Hasher()
+	assert.Implements(t, (*hasher.Hasher)(nil), h)
+	assert.IsType(t, &hasher.SHA512_256Hasher{}, h)
+}
+
+func TestNewSHA512_224Hasher(t *testing.T) {
+	h := NewSHA512_224Hasher()
+	assert.Implements(t, (*hasher.Hasher)(nil), h)
+	assert.IsType(t, &hasher.SHA512_224Hasher{}, h)
+}
+
+func TestNewSHA512Hasher(t *testing.T) {
+	h := NewSHA512Hasher()
+	assert.Implements(t, (*hasher.Hasher)(nil), h)
+	assert.IsType(t, &hasher.SHA512Hasher{}, h)
+}
+
+func TestNewSHA384Hasher(t *testing.T) {
+	h := NewSHA384Hasher()
+	assert.Implements(t, (*hasher.Hasher)(nil), h)
+	assert.IsType(t, &hasher.SHA384Hasher{}, h)
+}
+
+func TestNewSHA256Hasher(t *testing.T) {
+	h := NewSHA256Hasher()
+	assert.Implements(t, (*hasher.Hasher)(nil), h)
+	assert.IsType(t, &hasher.SHA256Hasher{}, h)
+}
+
+func TestNewSHA224Hasher(t *testing.T) {
+	h := NewSHA224Hasher()
+	assert.Implements(t, (*hasher.Hasher)(nil), h)
+	assert.IsType(t, &hasher.SHA224Hasher{}, h)
+}
+
+func TestNewSHA1Hasher(t *testing.T) {
+	h := NewSHA1Hasher()
+	assert.Implements(t, (*hasher.Hasher)(nil), h)
+	assert.IsType(t, &hasher.SHA1Hasher{}, h)
+}
+
+func TestNewMD5Hasher(t *testing.T) {
+	h := NewMD5Hasher()
+	assert.Implements(t, (*hasher.Hasher)(nil), h)
+	assert.IsType(t, &hasher.MD5Hasher{}, h)
+}
+
+func TestNewPlainHasher(t *testing.T) {
+	h := NewPlainHasher()
+	assert.Implements(t, (*hasher.Hasher)(nil), h)
+	assert.IsType(t, &hasher.PlainHasher{}, h)
+}
