aee876784a665a3e311d4e6ea5adca935139afa7
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2018-07-31 09:07:05 +0200

    added ContainsAtLeast validator

--- passwd/validator/validators.go
+++ passwd/validator/validators.go
@@ -1,6 +1,8 @@
 // Package validator is a set of functions to validate passwords
 package validator
 
+import "strings"
+
 // ValidateFunc defines a function to validate
 type ValidateFunc func(password string) bool
 
@@ -22,5 +24,21 @@ func MinLength(length int) ValidateFunc {
 func MaxLength(length int) ValidateFunc {
 	return ValidateFunc(func(password string) bool {
 		return len(password) <= length
+	})
+}
+
+// ContainsAtLeast returns a ValidateFunc that count occurrences of a chars and compares it with required value
+func ContainsAtLeast(chars string, occurrences int) ValidateFunc {
+	return ValidateFunc(func(password string) bool {
+		cnt := 0
+		aPassword := strings.Split(password, "")
+		for _, char := range strings.Split(chars, "") {
+			for _, pChar := range aPassword {
+				if char == pChar {
+					cnt++
+				}
+			}
+		}
+		return cnt >= occurrences
 	})
 }
--- passwd/validator/validators_test.go
+++ passwd/validator/validators_test.go
@@ -32,3 +32,14 @@ func ExampleMaxLength() {
 	// true
 	// true
 }
+
+func ExampleContainsAtLeast() {
+	passwordValidator := ContainsAtLeast("abcdefghijklmnopqrstuvwxyz", 4)
+	fmt.Println(passwordValidator("password"))
+	fmt.Println(passwordValidator("PASSWORD"))
+	fmt.Println(passwordValidator("passWORD"))
+	// Output:
+	// true
+	// false
+	// true
+}
