6edc8e04e7fc9aac7e02889694fcfa511ef8236b
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2018-08-09 15:41:14 +0200

    simplify test process

--- travis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ev
-cd passwd
-if [ "${COVERAGE}" = "true" ]; then
-  go test -v -covermode=count -coverprofile=coverage.out ./...
-  goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
-else
-  go test -v ./...
-fi
